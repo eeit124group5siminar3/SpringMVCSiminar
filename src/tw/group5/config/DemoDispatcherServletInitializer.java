eb4571f7ac33fe3d62a486e0a9b905c434ea8922
fuckup999 <71377371+fuckup999@users.noreply.github.com>
2020-11-03 12:49:59 +0800

    k1.5

--- src/tw/group5/config/DemoDispatcherServletInitializer.java
+++ src/tw/group5/config/DemoDispatcherServletInitializer.java
@@ -15,6 +15,7 @@ import javax.servlet.ServletResponse;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.internal.FilterConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
@@ -28,14 +29,15 @@ import tw.group5.util.OpenSessionInViewFilter;
 //Web.xml
 public class DemoDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-	@Override
-	protected Filter[] getServletFilters() {
-	CharacterEncodingFilter characterEncodingFilter=new CharacterEncodingFilter();
-	characterEncodingFilter.setEncoding("UTF-8");
-	characterEncodingFilter.setForceEncoding(true);
-	Filter openSessionInViewFilter=new OpenSessionInViewFilter();
-	return new Filter[] {characterEncodingFilter,openSessionInViewFilter};
-	}
+//	@Override
+//	protected Filter[] getServletFilters() {
+//	CharacterEncodingFilter characterEncodingFilter=new CharacterEncodingFilter();
+//	characterEncodingFilter.setEncoding("UTF-8");
+//	characterEncodingFilter.setForceEncoding(true);
+//	Filter openSessionInViewFilter=new OpenSessionInViewFilter();
+//	
+//	return new Filter[] {characterEncodingFilter,openSessionInViewFilter};
+//	}
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
@@ -58,25 +60,27 @@ public class DemoDispatcherServletInitializer extends AbstractAnnotationConfigDi
 //		super.registerContextLoaderListener(servletContext);
 //	}
 //	
-//	@Override
-//	public void onStartup(ServletContext servletContext) throws ServletException {
-//		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-//		rootContext.register(SpringMVCJavaConfig.class);
-//		rootContext.setServletContext(servletContext);
-//		ServletRegistration.Dynamic mvc = servletContext.addServlet("mvc", new DispatcherServlet(rootContext));
-//		mvc.setLoadOnStartup(1);
-//		mvc.addMapping("/");
-//		FilterRegistration.Dynamic filterRegistration = servletContext.addFilter("endcodingFilter", new CharacterEncodingFilter());
-//		filterRegistration.setInitParameter("encoding", "UTF-8");
-//		filterRegistration.setInitParameter("forceEncoding", "true");
-//		filterRegistration.addMappingForUrlPatterns(null, false, "/*");
-//		filterRegistration = servletContext.addFilter("OpenSessionInViewFilter", OpenSessionInViewFilter.class);
-//		//filterRegistration.setInitParameter("sessionFactoryBeanName", "sessionFactory");
-//		filterRegistration.addMappingForUrlPatterns(null, true, "/*");
-//		filterRegistration.addMappingForServletNames(null, true, "mvc");
-//
-//		servletContext.addListener(new ContextLoaderListener(rootContext));
-//	}
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+		rootContext.scan("/tw/group5/");
+		rootContext.refresh();
+		rootContext.register(SpringMVCJavaConfig.class);
+		rootContext.setServletContext(servletContext);
+		ServletRegistration.Dynamic mvc = servletContext.addServlet("mvc", new DispatcherServlet(rootContext));
+		mvc.setLoadOnStartup(1);
+		mvc.addMapping("/");
+		FilterRegistration.Dynamic filterRegistration = servletContext.addFilter("endcodingFilter", new CharacterEncodingFilter());
+		filterRegistration.setInitParameter("encoding", "UTF-8");
+		filterRegistration.setInitParameter("forceEncoding", "true");
+		filterRegistration.addMappingForUrlPatterns(null, false, "/*");
+		filterRegistration = servletContext.addFilter("OpenSessionInViewFilter", OpenSessionInViewFilter.class);
+		//filterRegistration.setInitParameter("sessionFactoryBeanName", "sessionFactory");
+		filterRegistration.addMappingForUrlPatterns(null, true, "/*");
+		filterRegistration.addMappingForServletNames(null, true, "mvc");
+
+		servletContext.addListener(new ContextLoaderListener(rootContext));
+	}
 
 
 }
