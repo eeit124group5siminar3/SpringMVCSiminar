31aae1fed0545a203cf8629a6fe5aae78d27ffe9
Minerva-Fu <71596530+Minerva-Fu@users.noreply.github.com>
2020-11-29 00:26:54 +0800

    20201128v1

--- src/tw/group5/active/controller/ActFarmerController.java
+++ src/tw/group5/active/controller/ActFarmerController.java
@@ -1,17 +1,14 @@
 package tw.group5.active.controller;
 
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 
 import java.sql.Blob;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
 //import javax.servlet.http.HttpServletRequest;
 //import javax.servlet.http.HttpServletResponse;
 import javax.sql.rowset.serial.SerialBlob;
@@ -23,7 +20,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
@@ -33,7 +29,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import tw.group5.active.model.ActFarmer;
-import tw.group5.active.model.Active;
 import tw.group5.active.model.Clock;
 import tw.group5.active.service.ActFarmerService;
 import tw.group5.member_SignUp.model.Member_SignUp;
---
+++ src/tw/group5/active/controller/ActSignController.java
@@ -0,0 +1,58 @@
+package tw.group5.active.controller;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import tw.group5.active.model.ActFarmer;
+import tw.group5.active.model.ActOrd;
+import tw.group5.active.service.ActFarmerService;
+import tw.group5.active.service.ActOrdService;
+import tw.group5.member_SignUp.model.Member_SignUp;
+
+@Controller
+public class ActSignController {
+	
+	@Autowired
+	private ActFarmerService actFarmerService;
+	
+	private ActOrdService actOrdService;
+	
+	
+	//查詢報名活動
+	
+	
+	//報名活動準備
+	@GetMapping(path = "/actSignPreInsert.do")
+	public String actSignPreInsert(Model model,
+			@RequestParam(value = "actId") Integer actId,
+			@SessionAttribute(value = "login_ok") Member_SignUp mb
+			) {
+		ActOrd actSign = new ActOrd();
+		ActFarmer actFarmer = actFarmerService.getActFarmer(actId);
+		model.addAttribute("actsigninert", actSign);//裝準備新增進去的資料
+		model.addAttribute("actFarmer", actFarmer);//裝要用到的活動資料
+		return "active/actSignupForm";		
+	}
+	
+	//報名活動(新增)
+	@PostMapping(value = "/actSignInsert.do")
+	public String actSignInsert(Model model,
+		@ModelAttribute(value = "actsigninert") ActOrd aoInsert,
+		@SessionAttribute(value = "login_ok") Member_SignUp mb) {
+			
+		actOrdService.insertActOrd(aoInsert);
+		return "redirect:/actFarmerList.do/{pageNo}";
+		
+	}
+	
+	
+
+}
